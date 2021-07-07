f7de0bdec984e474a8bc8ec9347bcb15dc146bbd
osipovmosu <86958991+osipovmosu@users.noreply.github.com>
2021-07-07 09:09:59 +0300

    Создание нового класса и расчёт расстояния между двумя точками

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -3,6 +3,19 @@ public class MyFirstProgram {
 	public static void main (String[] args)
 		{
 			System.out.println("Hello, world!");
+
+			Point p1 = new Point(3,2);
+			Point p2 = new Point(7,8);
+			System.out.println("Расстояние между двумя точками на плоскости с координатами (" + p1.a + ", " + p1.b + ")" + " и " + "(" + p2.a + ", " + p2.b + ")" + " равняется " + distance(p1,p2));
+
+
 		}
 
+
+		public static double distance (Point p1, Point p2){
+
+			return Math.sqrt(Math.pow((p2.a - p1.a),2) + Math.pow((p2.b - p1.b),2));
+
+
+		}
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,10 @@
+public class Point {
+
+    public double a;
+    public double b;
+
+    public Point(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+}
